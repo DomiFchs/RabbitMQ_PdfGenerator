08082e3c6521d79a1d3f2b9cda738335998d3332
Dominik Fuchs <D.FUCHS1@htlkrems.at>
2024-01-11 18:53:04 +0100

    Added ordered list :D

--- PDFGenerator/Services/PDFService.cs
+++ PDFGenerator/Services/PDFService.cs
@@ -4,41 +4,31 @@ using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using Document = QuestPDF.Fluent.Document;
 
-namespace PDFGenerator.Services; 
+namespace PDFGenerator.Services;
 
 public class PdfService {
     public void Generate(List<ToDo> todos) {
-        
-        Document.Create(container =>
-            {
-                container.Page(page =>
-                {
-                    page.Size(PageSizes.A4);
-                    page.Margin(2, Unit.Centimetre);
-                    page.DefaultTextStyle(x => x.FontSize(20));
+        Document.Create(container => {
+            container.Page(page => {
+                page.Size(PageSizes.A4);
+                page.Margin(2, Unit.Centimetre);
+                page.DefaultTextStyle(x => x.FontSize(20));
 
-                    page.Header()
-                        .Text("Todos")
-                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+                page.Header().Text("Todos").SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
 
-                    page.Content()
-                        .PaddingVertical(1, Unit.Centimetre)
-                        .Column(x =>
-                        {
-                            foreach (var todo in todos) {
-                                x.Item().Text(todo.Title).FontSize(20);
-                            }
-                        });
-                    page.Footer()
-                        .AlignCenter()
-                        .Text(x =>
-                        {
-                            x.Span("Page ");
-                            x.CurrentPageNumber();
+                page.Content().PaddingVertical(1, Unit.Centimetre).Column(column => {
+                    foreach (var todo in todos)
+                        column.Item().Row(row => {
+                            row.Spacing(5);
+                            row.AutoItem().Text($"{todos.IndexOf(todo)+1}. ");
+                            row.RelativeItem().Text(todo.Title);
                         });
                 });
-            })
-            .GeneratePdf($"Todos_{Guid.NewGuid()}.pdf");
+                page.Footer().AlignCenter().Text(x => {
+                    x.Span("Page ");
+                    x.CurrentPageNumber();
+                });
+            });
+        }).GeneratePdf($"Todos_{Guid.NewGuid()}.pdf");
     }
-    
 }
